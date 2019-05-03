dfa99bb8c229055e058a3c8893ecf6e2fafdedf2
Gurpreet Singh <gsrhar@gmail.com>
2019-05-03 13:01:22 -0700

    Update GameOfLife.java

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,3 +1,35 @@
+/*
+The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, 
+each of which is in one of two possible states, alive or dead, (or populated and unpopulated, respectively). 
+Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. 
+At each step in time, the following transitions occur:
+
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
+The application will take in one parameter a filename
+
+The file is attached to this email and will serve as the initial state for the application. 
+
+ 
+
+The input file is ascii characters and has the following format:
+
+Line 1: width height
+
+Line 2+:space separated values of 0 or 1
+
+ 
+The game's world is sized by the width and height. 
+
+
+*/
+
+
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
